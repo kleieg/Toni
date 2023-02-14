751adbfa086a2e0b0fc62770f16cd0109fc19919
kleieg <egbert.kleinhans@gmail.com>
2023-02-14 18:10:04 +0100

    Files gelöscht; platformio.ini nur ein env:

--- src/constants.hpp
+++ src/constants.hpp
@@ -3,15 +3,6 @@
 
 #include <Arduino.h>
 
-// Select the right PCB by uncommenting one of the following lines
-// Bitte die passende Platine durch entfernen der Kommentare in einer der folgenden Zeilen auswählen
-//#define TonUINO_Classic
-//#define ALLinONE
-//#define ALLinONE_Plus
-
-// uncomment the below line to enable five button support (already enabled for AiO and AiO+)
-// um dedizierte Lauter-/Leiserknöpfe zu haben bitte die nächste Zeile auskommentieren (Standard bei AiO und AiO+)
-//#define FIVEBUTTONS
 
 // uncomment the below line to enable serial input as additional command source
 // um den Serial Monitor als zusätzliche Kommandoquelle zu haben bitte in der nächste Zeile den Kommentar entfernen
@@ -47,13 +38,8 @@ inline constexpr uint8_t   buttonPausePin  = A0;
 inline constexpr uint8_t   buttonUpPin     = A1;
 inline constexpr uint8_t   buttonDownPin   = A2;
 
-#ifdef FIVEBUTTONS
-inline constexpr uint8_t   buttonFourPin   = A3;
-inline constexpr uint8_t   buttonFivePin   = A4;
-#endif
 
-//inline constexpr levelType buttonPinType   = levelType::activeLow;
-inline constexpr levelType buttonPinType   = levelType::activeHigh;
+inline constexpr levelType buttonPinType   = levelType::activeHigh;      // @@klh geändert von activeLow auf activeHigh wegen Sensortasten !!
 inline constexpr uint32_t  buttonDbTime    = 25; // Debounce time in milliseconds (default 25ms)
 
 // ####### chip_card ###################################
@@ -83,105 +69,5 @@ inline constexpr levelType     shutdownPinType = levelType::activeHigh;
 inline constexpr uint8_t       openAnalogPin   = A7;
 inline constexpr unsigned long cycleTime       = 50;
 #endif /* TonUINO_Classic */
-
-#ifdef ALLinONE_Plus
-// ####### buttons #####################################
-
-#define FIVEBUTTONS
-
-inline constexpr uint32_t  buttonLongPress = 1000; // timeout for long press button in ms
-inline constexpr uint8_t   buttonPausePin  = A0;
-inline constexpr uint8_t   buttonUpPin     = A2;
-inline constexpr uint8_t   buttonDownPin   = A1;
-
-#ifdef FIVEBUTTONS
-inline constexpr uint8_t   buttonFourPin   = A4;
-inline constexpr uint8_t   buttonFivePin   = A3;
-#endif
-
-inline constexpr levelType buttonPinType   = levelType::activeLow;
-inline constexpr uint32_t  buttonDbTime    = 25; // Debounce time in milliseconds (default 25ms)
-
-// ####### chip_card ###################################
-
-inline constexpr uint32_t cardCookie      = 0x1337b347;
-inline constexpr uint8_t  cardVersion     = 0x02;
-inline constexpr byte     mfrc522_RSTPin  = 11;
-inline constexpr byte     mfrc522_SSPin   =  7;
-inline constexpr uint8_t  cardRemoveDelay =  3;
-
-// ####### mp3 #########################################
-
-#define DFPlayerUsesHardwareSerial
-#define CHECK_MISSING_ONPLAYFINISHED
-
-inline constexpr uint8_t        maxTracksInFolder        = 255;
-// inline constexpr uint8_t        dfPlayer_receivePin      =  2;
-// inline constexpr uint8_t        dfPlayer_transmitPin     =  3;
-inline constexpr uint8_t        dfPlayer_busyPin         = 13;
-inline constexpr levelType      dfPlayer_busyPinType     = levelType::activeHigh;
-inline constexpr unsigned long  dfPlayer_timeUntilStarts = 500;
-inline constexpr HardwareSerial &dfPlayer_serial         = Serial3;
-// ####### tonuino #####################################
-
-inline constexpr uint8_t       shutdownPin      = 27;
-inline constexpr levelType     shutdownPinType  = levelType::activeLow;
-inline constexpr uint8_t       ampEnablePin     = 19;
-inline constexpr levelType     ampEnablePinType = levelType::activeLow;
-inline constexpr uint8_t       usbAccessPin     = 20;
-inline constexpr levelType     usbAccessPinType = levelType::activeHigh;
-inline constexpr uint8_t       openAnalogPin    = A7;
-inline constexpr unsigned long cycleTime        = 50;
-#endif /* ALLinONE_Plus */
-
-#ifdef ALLinONE
-// ####### buttons #####################################
-
-#define FIVEBUTTONS
-
-inline constexpr uint32_t  buttonLongPress = 1000; // timeout for long press button in ms
-inline constexpr uint8_t   buttonPausePin  = A0;
-inline constexpr uint8_t   buttonUpPin     = A2;
-inline constexpr uint8_t   buttonDownPin   = A1;
-
-#ifdef FIVEBUTTONS
-inline constexpr uint8_t   buttonFourPin   = A4;
-inline constexpr uint8_t   buttonFivePin   = A3;
-#endif
-
-inline constexpr levelType buttonPinType   = levelType::activeLow;
-inline constexpr uint32_t  buttonDbTime    = 25; // Debounce time in milliseconds (default 25ms)
-
-// ####### chip_card ###################################
-
-inline constexpr uint32_t cardCookie      = 0x1337b347;
-inline constexpr uint8_t  cardVersion     = 0x02;
-inline constexpr byte     mfrc522_RSTPin  =  9;
-inline constexpr byte     mfrc522_SSPin   = 10;
-inline constexpr uint8_t  cardRemoveDelay =  3;
-
-// ####### mp3 #########################################
-
-#define DFPlayerUsesSoftwareSerial
-#define CHECK_MISSING_ONPLAYFINISHED
-
-inline constexpr uint8_t       maxTracksInFolder        = 255;
-inline constexpr uint8_t       dfPlayer_receivePin      = 2;
-inline constexpr uint8_t       dfPlayer_transmitPin     = 3;
-inline constexpr uint8_t       dfPlayer_busyPin         = 4;
-inline constexpr levelType     dfPlayer_busyPinType     = levelType::activeHigh;
-inline constexpr unsigned long dfPlayer_timeUntilStarts = 500;
-
-// ####### tonuino #####################################
-
-inline constexpr uint8_t       shutdownPin      = 7;
-inline constexpr levelType     shutdownPinType  = levelType::activeLow;
-inline constexpr uint8_t       ampEnablePin     = 8;
-inline constexpr levelType     ampEnablePinType = levelType::activeLow;
-inline constexpr uint8_t       usbAccessPin     = A5;
-inline constexpr levelType     usbAccessPinType = levelType::activeHigh;
-inline constexpr uint8_t       openAnalogPin    = A0;
-inline constexpr unsigned long cycleTime        = 50;
-#endif /* ALLinONE */
 
 #endif /* SRC_CONSTANTS_HPP_ */
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,54 @@
-#ifdef __IN_VS_CODE__
+
 #include "Arduino.h"
 
 void setup();
 void loop();
 
-#include "../TonUINO-TNG.ino"
+#include "tonuino.hpp"
 
-#endif
+#include "settings.hpp"
+#include "mp3.hpp"
+#include "buttons.hpp"
+#include "logger.hpp"
+#include "constants.hpp"
+
+/*
+   _____         _____ _____ _____ _____
+  |_   _|___ ___|  |  |     |   | |     |
+    | | | . |   |  |  |-   -| | | |  |  |
+    |_| |___|_|_|_____|_____|_|___|_____|
+    TonUINO Version 3.1 - refactored by Boerge1
+
+    created by Thorsten Voß and licensed under GNU/GPL.
+    Information and contribution at https://tonuino.de.
+*/
+
+/*
+Please select your TonUINO PCB (Classic, AiO or AiO+) in file constants.hpp
+
+Bitte wählt eure TonUINO-Platine (Classic, AiO oder AiO+) in der Datei constants.hpp
+*/
+
+void setup()
+{
+  Serial.begin(115200);
+
+  // Dieser Hinweis darf nicht entfernt werden
+  LOG(init_log, s_error, F("\n _____         _____ _____ _____ _____"));
+  LOG(init_log, s_error, F("|_   _|___ ___|  |  |     |   | |     |"));
+  LOG(init_log, s_error, F("  | | | . |   |  |  |-   -| | | |  |  |"));
+  LOG(init_log, s_error, F("  |_| |___|_|_|_____|_____|_|___|_____|"));
+  LOG(init_log, s_error, F("TonUINO Version 3.1 - refactored by Boerge1\n"));
+  LOG(init_log, s_error, F("created by Thorsten Voß and licensed under GNU/GPL."));
+  LOG(init_log, s_error, F("Information and contribution at https://tonuino.de.\n"));
+  LOG(init_log, s_error, F("V3.1.2 06.02.23\n"));
+
+  Tonuino::getTonuino().setup();
+}
+
+void loop()
+{
+  Tonuino::getTonuino().loop();
+}
+
+
